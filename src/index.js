94b25a789a271abbe913faf57f48727460c208ef
Kory Newton <korybnewton@gmail.com>
2019-09-09 17:14:07 -0700

    add dropdown ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import Tooltip from './ui/tooltip';
+import Dropdown from './ui/dropdown';
 
 // create tooltip
-
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
